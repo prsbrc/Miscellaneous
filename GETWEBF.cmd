5aebd29c924668c731357d49f51ee68e121c66cd
Christian Brunner <31685593+prsbrc@users.noreply.github.com>
2019-07-05 11:17:33 +0200

    seperate folders

--- GETWEBF.cmd
+++
@@ -1,9 +0,0 @@
-             CMD        PROMPT('Download file onto ifs') +
-                          TEXT('Download file onto ifs') +
-                          ALLOW(*ALL) MODE(*ALL) ALWLMTUSR(*NO) +
-                          AUT(*EXCLUDE)
-
-             PARM       KWD(URL) TYPE(*CHAR) LEN(128) MIN(1) +
-                          CHOICE('Valid http-url') PROMPT('URL')
-             PARM       KWD(PATH) TYPE(*PNAME) LEN(128) MIN(1) +
-                          PROMPT('IFS-path')
--- sleep.rpgle
+++
@@ -1,5 +0,0 @@
-      /if not defined (#API_SLEEP)
-      /define #API_SLEEP
-     D Sleep           PR            10U 0 EXTPROC( 'sleep' )
-     D  Seconds                      10U 0  VALUE
-      /endif
